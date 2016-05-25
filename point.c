bf3fef0c76abb85b7bebb5ab09d48032d7e35b59
gen <b1014209@fun.ac.jp>
2016-05-25 15:49:24 +0900

    add some functions and structs for Extended Twisted Edwards Coordinates

--- point.c
+++ point.c
@@ -5,6 +5,7 @@
  * 更新履歴
  * 2014/10/30 新規作成
  * 2014/11/02 引数の一部をconstに変更
+ * 2016/05/25 Projective座標系からExtended Twisted Edwards Coodinate座標系への変換を追加
  *
  */
 
@@ -60,3 +61,27 @@ void protoaff(AFFINE_POINT R, const PROJECTIVE_POINT P, const mpz_t N)
 	mpz_clear(inv);
 }
 
+void protoext(EXTENDED_POINT R, const PROJECTIVE_POINT P, const mpz_t N)
+{
+	mpz_set(R_X, P_X);
+	mpz_set(R_Y, P_Y);
+	mpz_set(R_Z, P_Z);
+
+	mpz_t inv;
+	mpz_init(inv);
+
+	mpz_invert(inv, P->Z, N);
+	
+	mpz_mul(R->T, P->X, P->Y);
+	mpz_mul(R->T, R->T, inv);
+	mpz_mod(R->T, R->T, N);
+
+	mpz_clear(inv);
+}
+
+void exttopro(PROJECTIVE_POINT R, const EXTENDED_POINT P, const mpz_t N)
+{
+	mpz_set(R->X, P->X);
+	mpz_set(R->Y, P->Y);
+	mpz_set(R->Z, P->Z);
+}
